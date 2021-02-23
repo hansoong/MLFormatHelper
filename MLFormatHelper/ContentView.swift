d9122ed6fe0ff191f0479fa66f1d5a0bb411ab15
hansoong <hansoong@gmail.com>
2021-02-23 15:00:48 +0800

    initial git

---
+++ MLFormatHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MLFormatHelper
+//
+//  Created by Choong Han Soong on 23/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MLFormatHelper/MLFormatHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLFormatHelperApp.swift
+//  MLFormatHelper
+//
+//  Created by Choong Han Soong on 23/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MLFormatHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
